c67f194ab130f0d4c1a874c30fab34b523e5bf7a
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-07-21 10:40:39 -0300

    Better audio managment

--- modules/assets.lua
+++ modules/assets.lua
@@ -1,36 +1,77 @@
 local assets = {
     images = {},
     fonts = {},
-    audio = {}
+    sfx = {},
+    voicelines = {},
+    songs = {}
 }
 
-function assets.loadImage(name, path)
-    assets.images[name] = love.graphics.newImage(path)
+-- Loaders for each type of asset in the game
+
+function assets.loadImage(name, file)
+    assets.images[name] = love.graphics.newImage('assets/images/' .. file)
 end
 
-function assets.loadFont(name, path, size)
-    assets.fonts[name] = love.graphics.loadFont(path, size)
+function assets.loadFont(name, file, size)
+    assets.fonts[name] = love.graphics.loadFont('assets/fonts/' .. file, size)
 end
 
-function assets.loadAudio(name, path, isOST)
-    local type = 'static'
-    if isOST then
-        type = 'stream'
+function assets.loadSfx(name, file)
+    assets.sfx[name] = love.audio.newSource('assets/sfx/' .. file, 'static')
+end
+
+function assets.loadSong(name, file)
+    assets.songs[name] = love.audio.newSource('assets/songs/' .. file, 'stream')
+end
+
+function assets.loadVoiceLine(name, file)
+    assets.voicelines[name] = love.audio.newSource('assets/voicelines/' .. file, 'static')
+end
+
+-- Helper to stop an specific type of audio
+function assets.stopAudio(type)
+    if assets[type] then
+        for _, source in pairs(assets[type]) do
+            source:stop()
+        end
     end
-    assets.audio[name] = love.audio.newSource(path, type)
-    assets.audio[name]:setVolume(1)
 end
 
-function assets.load()
-    assets.loadImage('tile', '/assets/images/test.png')
-    assets.loadImage('spike', '/assets/images/spike.png')
-    assets.loadImage('goal', '/assets/images/goal.png')
+-- Helper to set volume for a whole type of audio
+function assets.setVolume(type, vol)
+    if assets[type] then
+        for _, source in pairs(assets[type]) do
+            source:setVolume(vol)
+        end
+    end
+end
 
-    love.audio.setVolume(1)
-    assets.loadAudio('intro', '/assets/audio/intro.wav', false)
-    assets.loadAudio('oopsie', '/assets/audio/oopsie.wav', false)
-    assets.loadAudio('loser', '/assets/audio/loser.wav', false)
-    assets.loadAudio('cmon', '/assets/audio/cmon.wav', false)
+-- Helper to see if there's any audio of a type playing in a given moment
+function assets.isPlayingAny(type)
+    -- Avoids nil related crashes
+    if not assets[type] then return false end
+
+    -- Does the verification
+    for _, source in pairs(assets[type]) do
+        if source:isPlaying() then
+            return true
+        end
+    end
+    return false
+end
+
+-- Loads every game asset
+function assets.load()
+    assets.loadImage('tile', 'test.png')
+    assets.loadImage('spike', 'spike.png')
+    assets.loadImage('goal', 'goal.png')
+
+    assets.loadVoiceLine('intro', 'intro.wav')
+    assets.loadVoiceLine('oopsie', 'oopsie.wav')
+    assets.loadVoiceLine('loser', 'loser.wav')
+    assets.loadVoiceLine('cmon', '/cmon.wav')
+
+    assets.loadSong('planetX', 'Imphenzia - Discovery of Planet X.ogg')
 end
 
 return assets
