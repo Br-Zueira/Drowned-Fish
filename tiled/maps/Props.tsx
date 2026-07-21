f3dfa5fb28bfff618316e2b3f9d39b399a2dd5ad
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-07-21 19:51:59 -0300

    Changed tileset embedding

---
+++ tiled/maps/Props.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Props" class="Saw" tilewidth="32" tileheight="32" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="Tile">
+  <image source="../../assets/images/test.png" width="32" height="32"/>
+ </tile>
+ <tile id="1" type="Placeholder">
+  <image source="../../assets/images/savepoint.png" width="32" height="32"/>
+ </tile>
+ <tile id="3" type="Spike">
+  <image source="../../assets/images/spike.png" width="32" height="16"/>
+ </tile>
+ <tile id="4" type="Goal">
+  <image source="../../assets/images/goal.png" width="32" height="32"/>
+ </tile>
+ <tile id="5" type="Saw">
+  <image source="../../assets/images/saw.png" width="32" height="32"/>
+ </tile>
+</tileset>
