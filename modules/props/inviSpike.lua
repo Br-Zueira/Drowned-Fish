e7b0170c0e617470ba1db68a9d152ec03bd7bb21
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-07-22 17:26:57 -0300

    Changed function of invispikes

--- modules/props/inviSpike.lua
+++ modules/props/inviSpike.lua
@@ -2,36 +2,36 @@ local prop = require 'modules.props.prop'
 local spike = require 'modules.props.spike'
 
 -- Invisible spike that only appears when player is close
----@class InviSpike : Prop
+---@class InviSpike : Spike
 ---@field x number X coordinate of spike
 ---@field y number Y coordinate of spike
 ---@field radius number Radius in which player must be close in order to reveal the spike
 ---@field degrees? number Number in degrees in order to make a rotated invispike
+---@field isInvisible boolean Makes the Invi of the InviSpike 
 local InviSpike = {}
 InviSpike.__index = InviSpike
-setmetatable(InviSpike, prop.Prop)
+setmetatable(InviSpike, spike)
 
 -- Creates a new InviSpike
 ---@param x number X coordinate of spike
 ---@param y number Y coordinate of spike
 ---@param radius number Radius in which player must be close in order to reveal the spike
----@param d? number Number in degrees in order to make a rotated invispike
+---@param d? number Degrees in order to make a rotated InviSpike
 ---@return InviSpike
 function InviSpike.new(x, y, radius, d)
-    y = y - TileSize/2
-    local instance = prop.Prop.new(x, y, TileSize, TileSize/2, { isImg=nil })
+    local instance = spike.new(x, y, d)
     ---@cast instance InviSpike
+    instance.isInvisible = true
     instance.radius = radius
-    instance.degrees = d or 0
     setmetatable(instance, InviSpike)
     return instance
 end
 
+-- Updates InviSpike
+---@param player Player The player instance
 function InviSpike:update(_, player)
     if prop.isPlayerInRadius(self, player, self.radius) then
-        self:delete()
-        local s = spike.new(self.x, self.y+TileSize/2)
-        s.degrees = self.degrees
+        self.isInvisible = false
     end
 end
 
