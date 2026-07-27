21a80e135d377e9436aa0b3234cc92163c634ff4
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-07-27 19:24:51 -0300

    Updated goal sprite and addes spring sprite

--- tiled/Props.tsx
+++ tiled/Props.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="Props" class="Saw" tilewidth="32" tileheight="32" tilecount="5" columns="0">
+<tileset version="1.10" tiledversion="1.12.2" name="Props" class="Saw" tilewidth="32" tileheight="32" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="Tile">
   <image source="../assets/images/test.png" width="32" height="32"/>
@@ -15,5 +15,8 @@
  </tile>
  <tile id="5" type="Saw">
   <image source="../assets/images/saw.png" width="32" height="32"/>
+ </tile>
+ <tile id="6" type="Spring">
+  <image source="../assets/images/spring.png" width="32" height="32"/>
  </tile>
 </tileset>
