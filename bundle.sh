b309aac4b51bbf7f52f55ae4b1b5dc65f804ffc2
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-08-04 21:19:21 -0300

    Added bash script to bundle game into .love file

---
+++ bundle.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail # Safety flags that quit at any error
+
+echo "Setting up variables"
+OUTPUT="drowned-fish.love" # Sets the final file name
+TARGETS=( # Files and folders to be included
+    "assets"
+    "libs"
+    "maps"
+    "modules"
+    "conf.lua"
+    "main.lua"
+)
+
+echo "Cleaning old bundles"
+rm -f "$OUTPUT" # Remove old build, if any
+
+echo "Creating new bundle"
+zip -rq "$OUTPUT" "${TARGETS[@]}" -x "*.git*" "*.DS_Store" # Zips the game
+
+echo "'$OUTPUT' bundle successfully finished"
