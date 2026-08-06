9ccada8962f4ee420ff95f503a77e0adc12ce628
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-08-05 21:43:08 -0300

    Moved source files  into src folder

--- bundle.sh
+++ bundle.sh
@@ -4,19 +4,13 @@ set -euo pipefail # Safety flags that quit at any error
 
 echo "Setting up variables"
 OUTPUT="out/drowned-fish.love" # Sets the final file name
-TARGETS=( # Files and folders to be included
-    "assets"
-    "libs"
-    "maps"
-    "modules"
-    "conf.lua"
-    "main.lua"
-)
+OUTPUT_ABS="$(realpath -m "$OUTPUT")"
+TARGET="src" # Source target of build
 
 echo "Cleaning old bundles"
 rm -f "$OUTPUT" # Remove old build, if any
 
 echo "Creating new bundle"
-zip -9 -rq "$OUTPUT" "${TARGETS[@]}" -x "*.git*" "*.DS_Store" # Zips the game
+(cd $TARGET && zip -9 -rq "$OUTPUT_ABS" . -x "*.git*" "*.DS_Store") # Zips the game
 
 echo "'$OUTPUT' bundle successfully finished"
--- tiled/Props.tsx
+++ tiled/Props.tsx
@@ -2,27 +2,27 @@
 <tileset version="1.10" tiledversion="1.12.2" name="Props" class="Saw" tilewidth="32" tileheight="32" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="Tile">
-  <image source="../assets/images/test.png" width="32" height="32"/>
+  <image source="../src/assets/images/test.png" width="32" height="32"/>
  </tile>
  <tile id="1" type="Placeholder">
-  <image source="../assets/images/placeholder.png" width="32" height="32"/>
+  <image source="../src/assets/images/placeholder.png" width="32" height="32"/>
  </tile>
  <tile id="3" type="Spike">
-  <image source="../assets/images/spike.png" width="32" height="16"/>
+  <image source="../src/assets/images/spike.png" width="32" height="16"/>
  </tile>
  <tile id="4" type="Goal">
-  <image source="../assets/images/goal.png" width="32" height="32"/>
+  <image source="../src/assets/images/goal.png"/>
  </tile>
  <tile id="5" type="Saw">
-  <image source="../assets/images/saw.png" width="32" height="32"/>
+  <image source="../src/assets/images/saw.png"/>
  </tile>
  <tile id="6" type="Spring">
-  <image source="../assets/images/spring.png" width="32" height="32"/>
+  <image source="../src/assets/images/spring.png"/>
  </tile>
  <tile id="7" type="Booster">
-  <image source="../assets/images/booster.png" width="32" height="32"/>
+  <image source="../src/assets/images/booster.png" width="32" height="32"/>
  </tile>
  <tile id="8" type="Laser">
-  <image source="../assets/images/laser.png" width="32" height="32"/>
+  <image source="../src/assets/images/laser.png"/>
  </tile>
 </tileset>
