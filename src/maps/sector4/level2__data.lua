5c2c7504b9e4dc1b925ea7f58f5a088f0b002bf2
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-08-16 16:50:42 -0300

    (Maybe) finished S4L2

--- src/maps/sector4/level2__data.lua
+++ src/maps/sector4/level2__data.lua
@@ -12,13 +12,38 @@ function data.whenLoaded()
     voicelines.add('oopsie', 6, 1, 4, true)
 end
 
-function data.whenReloaded() end
+local spinPoint = {}
+local sP = {}
+
+function data.whenReloaded()
+    spinPoint = {}
+    sP = {}
+end
+
 function data.update() end
+
+local levelTrigger = {}
+levelTrigger.__index = levelTrigger
+setmetatable(levelTrigger, props.Trigger)
+
+function levelTrigger:update(_, player)
+    if props.isPlayerInRadius(self, player, self.radius) then
+        self:delete()
+        if self.id == "speedup" then
+            spinPoint.speed = spinPoint.speed * 2
+            for _, tile in ipairs(sP) do
+                tile.rotationSpeed = 8
+                tile.isCounterclockwise = not tile.isCounterclockwise
+            end
+        end
+    end
+end
+
 function data.ObjHandler(obj)
     local p = obj.properties
     if obj.name == "MovSpinningPlat" then
-        local spinPoint = {}
-        local sP = props.SpinningPlat.new(
+        spinPoint = {}
+        sP = props.SpinningPlat.new(
             obj.x, obj.y,
             p.width, p.height,
             p.rotationSpeed, p.isCounterclockwise,
@@ -29,9 +54,22 @@ function data.ObjHandler(obj)
         )
         spinPoint = props.Prop.new(sP[1].rotationX, sP[1].rotationY, TileSize/2, TileSize/2, { isImg=nil })
         spinPoint.isTrigger = true
-        props.Moveable.set(spinPoint, spinPoint.x, spinPoint.y, spinPoint.x, TileSize*7, p.speed, false, false)
+        props.Moveable.set(spinPoint, spinPoint.x, spinPoint.y, spinPoint.x, TileSize*6, p.speed, false, false)
+    elseif obj.name == "Trigger" then
+        local t = props.Trigger.new(obj.x, obj.y, p.id, p.radius)
+        setmetatable(t, levelTrigger)
+    elseif obj.name == "FakeMoverSaw" then
+        local s = props.MoverSaw.new(obj.x, obj.y, p.endX, p.endY, p.speed, p.isOneWay, p.isSinglePass)
+        s.type = "Solid"
+        s.isCross = false
+    elseif obj.name == "DeadlyTile" then
+        obj.y = obj.y - TileSize
+        local t = props.Tile.new(obj.x, obj.y)
+        t.type = "Hazard"
+        t.isCross = true
     end
 end
+
 function data.MiscHandler(map) end
 
 return data
