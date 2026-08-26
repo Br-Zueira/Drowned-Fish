45512565edb7102e5160e0c22e0f08d0408866c7
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-08-25 21:16:27 -0300

    Feat: S4L5

--- src/maps/sector4/level5__data.lua
+++ src/maps/sector4/level5__data.lua
@@ -12,9 +12,47 @@ function data.whenLoaded()
     voicelines.add('oopsie', 6, 1, 4, true)
 end
 
-function data.whenReloaded() end
+local futureSaws = {}
+
+function data.whenReloaded()
+    futureSaws = {}
+end
+
 function data.update() end
-function data.ObjHandler(obj) end
+
+local levelTrigger = {}
+levelTrigger.__index = levelTrigger
+setmetatable(levelTrigger, props.Trigger)
+
+function levelTrigger:update(_, player)
+    if not props.isPlayerInRadius(self, player, self.radius) then return end
+    self:delete()
+    if self.id == "spawnSaws" then
+        local speed = 750
+        for i = #futureSaws, 1, -1 do
+            local fS = futureSaws[i]
+            props.MoverSaw.new(fS.x, -TileSize, fS.x, VH+TileSize, speed, true)
+            table.remove(futureSaws, i)
+        end
+    elseif self.id == "deleteMovs" then 
+        for _, p in ipairs(props.propList) do
+            if getmetatable(p) == props.MoverSaw then
+                p.isSinglePass = true
+            end
+        end
+    end
+end
+
+function data.ObjHandler(obj)
+    local p = obj.properties
+    if obj.name == "Trigger" then
+        local t = props.Trigger.new(obj.x, obj.y, p.id, p.radius)
+        setmetatable(t, levelTrigger)
+    elseif obj.name == "FutSaws" then
+        table.insert(futureSaws, obj)
+    end
+end
+
 function data.MiscHandler(map) end
 
 return data
