d8cb841ede9fa4aa18111ce694ed0d8515161985
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-09-04 22:52:20 -0300

    Feat: Added 'integer overflow' to S5L4

--- src/maps/sector5/level4__data.lua
+++ src/maps/sector5/level4__data.lua
@@ -12,9 +12,40 @@ function data.whenLoaded()
     voicelines.add('oopsie', 6, 1, 4, true)
 end
 
-function data.whenReloaded(player) end
-function data.update(dt, player) end
-function data.ObjHandler(obj) end
+-- TODO: Make tiles that fall (down or up) after the "buffer overflow" and when player is near
+
+local canOverflow = false
+
+function data.whenReloaded(player)
+    canOverflow = false
+end
+
+function data.update(dt, player)
+    if canOverflow then
+        player.levelDeaths = player.levelDeaths + math.ceil(100000000*dt)
+    end
+end
+
+local levelTrigger = {}
+levelTrigger.__index = levelTrigger
+setmetatable(levelTrigger, props.Trigger)
+
+function levelTrigger:update(_, player)
+    if not props.isPlayerInRadius(self, player, self.radius) then return end
+    self:delete()
+    if self.id == "overflow" then
+        canOverflow = true
+    end
+end
+
+function data.ObjHandler(obj)
+    local p = obj.properties
+    if obj.name == "Trigger" then
+        local t = props.Trigger.new(obj.x, obj.y, p.id, p.radius)
+        setmetatable(t, levelTrigger)
+    end
+end
+
 function data.MiscHandler(map) end
 
 return data
